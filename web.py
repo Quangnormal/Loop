b2500d8062494890bb8d8dd2fbabb02755b914a1
Quangnormal <quangnormal.dll@gmail.com>
2025-12-18 22:22:44 +0700

    Update web.py

--- web.py
+++ web.py
@@ -1,25 +1,22 @@
 from flask import Flask
-from main import run_job
+import main
 import threading
 
 app = Flask(__name__)
-LOCK = threading.Lock()
 
+lock = threading.Lock()
 
-@app.route("/")
-def index():
-    if LOCK.locked():
-        return "Job is running", 429
+@app.route("/", methods=["GET", "HEAD"])
+def trigger():
+    with lock:
+        status, text = main.run()
 
-    with LOCK:
-        run_job()
-
-    return "OK - request sent", 200
-
-
-@app.route("/health")
-def health():
-    return "alive", 200
+    return f"""
+    <h3>Triggered Minecraft Start</h3>
+    <b>Status:</b> {status}<br>
+    <b>Response:</b><br>
+    <pre>{text}</pre>
+    """, 200
 
 
 if __name__ == "__main__":
